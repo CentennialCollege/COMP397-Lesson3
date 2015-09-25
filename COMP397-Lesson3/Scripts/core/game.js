ff04ac6221d0979edf79e305cc778a75d3cee011
tsiliopoulos <tsiliopoulos@hotmail.com>
2015-09-25 17:20:46 -0400

    Added Button Images to project

--- COMP397-Lesson3/Scripts/core/game.js
+++ COMP397-Lesson3/Scripts/core/game.js
@@ -1,45 +1,33 @@
-﻿/// <reference path="../typings/jquery/jquery.d.ts" />
+/// <reference path="../typings/jquery/jquery.d.ts" />
 /// <reference path="../typings/stats/stats.d.ts" />
 /// <reference path="../typings/createjs-lib/createjs-lib.d.ts" />
 /// <reference path="../typings/easeljs/easeljs.d.ts" />
 /// <reference path="../typings/tweenjs/tweenjs.d.ts" />
 /// <reference path="../typings/soundjs/soundjs.d.ts" />
 /// <reference path="../typings/preloadjs/preloadjs.d.ts" />
-
 /// <reference path="../objects/label.ts" />
-
-
-
 // GLOBAL GAME FRAMEWORK VARIABLES
-var canvas: HTMLElement;
-var stage: createjs.Stage;
-var stats: Stats;
-
+var canvas;
+var stage;
+var stats;
 // Game Variables
-var helloLabel: objects.Label;
-var goodByeLabel: objects.Label;
-
-function init():void {
+var helloLabel;
+var goodByeLabel;
+function init() {
     console.log("Game Started...");
     canvas = document.getElementById("canvas"); // reference to canvas element
     stage = new createjs.Stage(canvas); // passing canvas to stage
     createjs.Ticker.setFPS(60); // set frame rate to 60 fps
     createjs.Ticker.on("tick", gameLoop); // update gameLoop every frame
     setupStats(); // sets up our stats counting
-
-
     main(); // call main game function
 }
-
 // Main Game Loop
-function gameLoop(event: createjs.Event): void {
+function gameLoop(event) {
     stats.begin(); // start counting
-
     stage.update(); // redraw/refresh stage every frame
-
     stats.end(); // stop counting
 }
-
 // Setup Game Stats
 function setupStats() {
     stats = new Stats();
@@ -49,13 +37,9 @@ function setupStats() {
     stats.domElement.style.top = "0px";
     document.body.appendChild(stats.domElement);
 }
-
-
 // This is where all the fun happens
-function main(): void {
+function main() {
     helloLabel = new objects.Label("Game Start", "60px Consolas", "#000000", 320, 240);
     stage.addChild(helloLabel); // add label to the stage
-
-
 }
- 
+//# sourceMappingURL=game.js.map
